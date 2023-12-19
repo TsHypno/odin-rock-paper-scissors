47a519d9ebd82992eaff204587d4727e1620a336
Ricardo Noel Navarro <r.noel.navarro@gmail.com>
2023-12-19 14:32:23 -0600

    Corrected version of script.js

--- script.js
+++ script.js
@@ -1,5 +1,47 @@
-function playRockPaperScissors(playerChoice, computerChoice) {
+var playerScore = 0;
+var computerScore = 0;
 
+function game() {
+    let outcome;
+    for(let round = 1; round < 6; round++) {
+        console.log("Round " + round);
+        let computerChoice = getComputerChoice();
+        outcome = playRockPaperScissors(playerChoice, computerChoice);
+        if (outcome === 0) {
+            console.log("It's a draw")
+        } else if (outcome === 1) {
+            playerScore++;
+            console.log("Player wins");
+        } else if (outcome ===2) {
+            computerScore++;
+            console.log("Computer wins");
+        }
+    }
+    if (playerScore > computerScore) {
+        console.log("Player wins with a score of " + playerScore);
+    } else {
+        console.log("Computer wins with a score of " + computerScore);
+    }
+}
+
+function playRockPaperScissors(playerChoice, computerChoice) {
+    let convertedPlayerChoice = convertPlayerChoice(playerChoice);
+    if (playerChoice === computerChoice) {
+        return 0;
+    } else if ((playerChoice === "rock" && computerChoice === "scissors") || 
+                (playerChoice === "scissors" && computerChoice === "paper") || 
+                (playerChoice === "paper" && computerChoice === "rock")) {
+        return 1;
+    } else if ((playerChoice === "rock" && computerChoice === "paper") || 
+                (playerChoice === "scissors" && computerChoice === "rock") || 
+                (playerChoice === "paper" && computerChoice === "scissors")) {
+        return  2;
+    }
+}
+
+function convertPlayerChoice(playerChoice) {
+    let convertedPlayerChoice = playerChoice.toLowerCase();
+    return convertedPlayerChoice;
 }
 
 function getComputerChoice() {
@@ -23,9 +65,7 @@ function randomIntFromInterval(min, max) { // min and max included
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
-const playerChoice = "rock";
-const computerChoice = getComputerChoice();
+let playerChoice = "rock";
+//const computerChoice = getComputerChoice();
 
-
-//playRockPaperScissors(playerChoice, )
-
+game();
